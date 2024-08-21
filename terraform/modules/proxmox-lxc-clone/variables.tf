31dfdf31d7cb48e34e84b4e5c83caa58f57c50c2
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2024-08-21 23:37:12 +0900

    Terraform: LXC cloneモジュール作成

---
+++ terraform/modules/proxmox-lxc-clone/variables.tf
@@ -0,0 +1,14 @@
+variable "target_node" {
+  description = "(required) - A string containing the cluster node name."
+  type = string
+}
+
+variable "hostname" {
+  description = "Specifies the host name of the container."
+  type = string
+}
+
+variable "clone" {
+  description = "The lxc vmid to clone"
+  type = string
+}
